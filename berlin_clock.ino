6ce15f9040c8340155126b30e53638da0892e285
Noan Mousy <4sstylz@protonmail.ch>
2017-08-27 18:58:48 +0200

    Testing the leds…

---
+++ berlin_clock.ino
@@ -0,0 +1,74 @@
+
+void setup()   {
+
+  Serial.begin(38400);
+  Serial.println("Hello World");
+  startWithSomeFlash();
+
+  // initialize the digitals pin as an outputs
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+
+// the loop() method runs over and over again,
+void loop()
+{
+
+  allHigh();
+  delay(1000);
+  allLow();
+  delay(1000);
+  Serial.println("Iteration…");
+}
+
+void allHigh() {
+  digitalWrite(0, HIGH);
+  digitalWrite(1, HIGH);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+}
+
+
+void allLow() {
+  digitalWrite(0, LOW);
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+}
+
+void startWithSomeFlash() {
+  pinMode(13, OUTPUT);
+  delay(200);
+  digitalWrite(13, HIGH);
+  delay(200);
+  digitalWrite(13, LOW);
+  delay(200);
+  digitalWrite(13, HIGH);
+  delay(200);
+  digitalWrite(13, LOW);
+  delay(200);
+  digitalWrite(13, HIGH);
+  delay(200);
+  digitalWrite(13, LOW);
+}
