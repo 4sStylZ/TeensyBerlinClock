f3bf199128111dabe6ae6b3ba2c45958861f0afd
Noan Mousy <4sstylz@protonmail.ch>
2017-08-30 23:07:04 +0200

    Fix the clock, add the alarm at 9h30.

--- berlin_clock.ino
+++ berlin_clock.ino
@@ -1,8 +1,19 @@
-#include <Metro.h> // Include the Metro library
-#include <TimeLib.h> // Include Time library.
+// Metro is used for check the time each second.
+// This is the best way for not working with delay().
+#include <Metro.h>
+
+// Time is the library wich provide some method like setTime and
+// others getters like hour() / minutes().
+#include <TimeLib.h>
+
+// Time alarm call your method at a special time based on the Time
+// library.
+#include <TimeAlarms.h>
 
 Metro serialMetro = Metro(1000);
 time_t currentTime;
+bool isAlarmOn = false;
+AlarmId id;
 
 /**
  * Auto invoked setup function.
@@ -13,17 +24,69 @@ void setup()   {
 
     // Start the serial monitoring for debug purpose.
     Serial.begin(38400);
+    Serial.println("Start Berlin clock");
 
     // Initialize the digitals pin as an outputs
     initLedsOutput();
 
     // Blink some flash just for see if the device is started even
     // if the serial monitoring is not set-up.
-    startWithSomeFlash();
+    partyHard(3);
 
-    // set time to Saturday 8:29:00am Jan 1 2011.
-    setTime(8,29,0,1,1,11);
+    initializeTime();
 
+    // Declare the alamr each day except weekend days.
+    setDailyScrumAlarm(9, 30, 00);
+}
+
+/**
+ * Method wich initialise the time and date.
+ *
+ * @return {void}
+ */
+void initializeTime() {
+
+    // Initialize time.
+    setTime(
+         9, // Hours   (HH)
+        25, // Minutes (MM)
+        00, // Seconds (SS)
+        31, // Days    (DD)
+         8, // Months  (MM)
+        17  // Years   (YY)
+    );
+}
+
+/**
+ * Display flash at 9:30 each day except saturday and sunday.
+ *
+ * @parameters int hours, minutes, seconds.
+ *
+ * @return {void}
+ */
+void setDailyScrumAlarm(int hours, int minutes, int seconds) {
+    Alarm.alarmRepeat(9,30,0, displayScrumAlarm);
+    // Alarm.alarmRepeat(dowMonday    , hours , minutes, seconds, displayScrumAlarm);
+    // Alarm.alarmRepeat(dowTuesday   , hours , minutes, seconds, displayScrumAlarm);
+    // Alarm.alarmRepeat(dowWednesday , hours , minutes, seconds, displayScrumAlarm);
+    // Alarm.alarmRepeat(dowThursday  , hours , minutes, seconds, displayScrumAlarm);
+    // Alarm.alarmRepeat(dowFriday    , hours , minutes, seconds, displayScrumAlarm);
+    Serial.println("Daily scrum setted");
+}
+
+/**
+ * Display the Scrum alarm.
+ *
+ * (Method coded because alarmRepeat doesn’t accept method with
+ * parameters).
+ *
+ * @return {void}
+ */
+void displayScrumAlarm() {
+    Serial.println("Daily scrum setted");
+    isAlarmOn = true;
+    partyHard(20);
+    isAlarmOn = false;
 }
 
 /**
@@ -36,26 +99,61 @@ void loop()
     ticTac();
 }
 
+/**
+ * Tic, tac, tic… Et caetera !
+ *
+ * @return {void}
+ */
 void ticTac() {
 
     // Check if one second is passed.
     if (serialMetro.check() == 1) {
-        Serial.print(hour());
-        printDigits(minute());
-        printDigits(second());
-        Serial.println();
 
-        displaySecond();
-        displayHours();
-        // displayHour();
-        // displayMinuteFirstRow();
+        // Display the time in serie port.
+        displaySerialTime();
 
+        // Trigger the alarm for checking if there is one.
+        Alarm.delay(1000);
+
+        // If the alarm is off then display time on leds.
+        if (!isAlarmOn) {
+
+            // Blink the led of each zone.
+            displaySecond();
+            displayHours();
+            displayHour();
+            displayMinutes();
+            displayMinute();
+        }
     }
 }
 
+/**
+ * Display the time in serial port.
+ *
+ * @return {void}
+ */
+void displaySerialTime() {
+    Serial.print(day());
+    Serial.print("/");
+    Serial.print(month());
+    Serial.print("/");
+    Serial.print(year());
+    Serial.print(" ");
+    Serial.print(hour());
+    printDigits(minute());
+    printDigits(second());
+    Serial.println();
+}
+
+/**
+ * Display the first hour row.
+ *
+ * @return {[type]} [description]
+ */
 void displayHours() {
     for (int i = 1; i <= 4; i++) {
-        if (hour()%5) {
+        if (i <= floor(hour() / 5)) {
             digitalWrite(i, HIGH);
         } else {
             digitalWrite(i, LOW);
@@ -63,9 +161,14 @@ void displayHours() {
     }
 }
 
+/**
+ * Display the second hour row for hour unit.
+ *
+ * @return {void}
+ */
 void displayHour() {
     for (int i = 1; i <= 4; i++) {
-        if (hour()%5) {
+        if (i <= hour() % 5) {
             digitalWrite(i + 4, HIGH);
         } else {
             digitalWrite(i + 4, LOW);
@@ -73,8 +176,41 @@ void displayHour() {
     }
 }
 
+/**
+ * Display the first minute row.
+ *
+ * @return {void}
+ */
+void displayMinutes() {
+    for (int i = 1; i <= 12; i++) {
+        if (i <= floor(minute() / 5)) {
+            digitalWrite(i + 8, HIGH);
+        } else {
+            digitalWrite(i + 8, LOW);
+        }
+    }
+}
 
+/**
+ * Display the second minute row.
+ *
+ * @return {void}
+ */
+void displayMinute() {
+    for (int i = 1; i <= 4; i++) {
+        if (i <= minute() % 5) {
+            digitalWrite(i + 19, HIGH);
+        } else {
+            digitalWrite(i + 19, LOW);
+        }
+    }
+}
 
+/**
+ * Blink the second led at one time / second.
+ *
+ * @return {void}
+ */
 void displaySecond() {
     if (second()%2) {
         digitalWrite(0, HIGH);
@@ -83,25 +219,16 @@ void displaySecond() {
     }
 }
 
-void displayMinuteFirstRow() {
-    for (int i = 1; i <= 4; i++) {
-        if (minute()%5) {
-            digitalWrite(i + 1, HIGH);
-        } else {
-            digitalWrite(i + 1, LOW);
-        }
-    }
-}
-
 /**
- * Print some flash when the device start.
+ * Print some flash.
  *
- * @return {[type]} [description]
+ * @param {int} flashNumber
+ *
+ * @return {void}]
  */
-void startWithSomeFlash() {
+void partyHard(int flashNumber) {
 
-    Serial.println("Start Berlin clock");
-    for (int i = 0; i <= 3; i++) {
+    for (int i = 0; i <= flashNumber; i++) {
         powerAllLights(true);
         delay(100);
         powerAllLights(false);
@@ -110,7 +237,7 @@ void startWithSomeFlash() {
 }
 
 /**
- * Initialize all the leds output.
+ * Initialize all the leds in output mode.
  *
  * @return {void}
  */
@@ -123,13 +250,13 @@ void initLedsOutput() {
 /**
  * Blink off or blink on all the leds.
  *
- * @param boolean power HIGH or LOW
+ * @param boolean power, true for HIGH or false for LOW
  *
  * @return void
  */
 void powerAllLights(boolean power) {
     int i;
-    for (i = 1; i <= 24; i++) {
+    for (i = 0; i <= 24; i++) {
         if (power == true) {
             digitalWrite(i, HIGH);
         } else {
@@ -139,11 +266,12 @@ void powerAllLights(boolean power) {
 }
 
 /**
- * Prints two digits even if the parameter have only one caractere…
+ * View-helper who print two digits even if the parameter have only
+ * one caracter…
  *
- * @param {[type]} int digits [description]
+ * @param {int} int digits wich are minutes or second
  *
- * @return {[type]} [description]
+ * @return {void}
  */
 void printDigits(int digits) {
     Serial.print(":");
